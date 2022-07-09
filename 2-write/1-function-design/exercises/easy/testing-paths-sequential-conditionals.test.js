e35c2a31bb64be2f2e3629b217febe42a16b8f25
Maria <naplesaries2014@gmail.com>
2022-07-09 20:37:58 +0200

    my solutions

--- 2-write/1-function-design/exercises/easy/testing-paths-sequential-conditionals.test.js
+++ 2-write/1-function-design/exercises/easy/testing-paths-sequential-conditionals.test.js
@@ -15,24 +15,29 @@
  */
 
 // -------- your solutions --------
-
-for (const solution of [secretSolution]) {
+const convertBoolean = (a = false, b = false) => { let c=''
+  return  (a,b)? '10' : a,b? '00':!a,b?'01':'11'
+}
+for (const solution of [convertBoolean]) {
   // this function only 4 possible combinations of arguments
   //  it's possible test them all and have 100% confidence in the function
   describe(solution.name + ': converts two booleans to binary', () => {
     it('true, true --> "11"', () => {
-      const actual = solution(_, _);
-      expect(actual).toEqual(_);
+      const actual = solution(true,true);
+      expect(actual).toEqual('11');
     });
     it('true, false --> "10"', () => {
-      const actual = _;
+      const actual = solution(true,false);
       expect(actual).toEqual('10');
     });
     it('false, true --> "01"', () => {
-      const actual = _;
-      _;
+      const actual = solution(false,true);
+      expect(actual).toEqual("01");
     });
-    it('_', () => {});
+    it('false,false', () => {
+  const actual = solution(false, false);
+  expect(actual).toEqual("00");
+    });
   });
 }
 
